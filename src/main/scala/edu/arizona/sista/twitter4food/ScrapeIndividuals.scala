cc190e9c323804400692445c440d95a6a186daa2
Dane Bell <dane@email.arizona.edu>
2015-09-18 14:03:58 -0700

    Convert Java List to Scala Buffer for recursion

--- src/main/scala/edu/arizona/sista/twitter4food/ScrapeIndividuals.scala
+++ src/main/scala/edu/arizona/sista/twitter4food/ScrapeIndividuals.scala
@@ -7,221 +7,223 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.PrintWriter
 import java.util.Date
+import scala.collection.JavaConversions.asScalaBuffer
 
 class ScrapeIndividuals {
   val SLEEP = 3100
 
   def dateToString(d: Date): String = if(d == null) "NIL" else d.toString
 
-  def placeToString(p: Place): String =  {
-    if(p == null) return "NIL"
+  def placeToString(p: Place): String = {
+    if (p == null) return "NIL"
     val os = new StringBuilder
     os.append(c(p.getPlaceType()))
     os.append("/" + c(p.getFullName()))
     os.append("/" + c(p.getCountryCode()))
     os.append("/" + c(p.getBoundingBoxType()))
     val gs = p.getBoundingBoxCoordinates
-    if(gs != null) {
+    if (gs != null) {
       for {
         i <- 1 until gs.length
         j <- 1 until gs(i).length
       } {
         os.append("/" + geoLocationToString(gs(i)(j)))
       }
-      os.toString
     }
+    os.toString
+  }
 
-    def geoLocationToString(g: GeoLocation): String = {
-      if(g == null) return "NIL"
-      c(g.getLatitude.toString) + "|" + c(g.getLongitude.toString)
-    }
+  def geoLocationToString(g: GeoLocation): String = {
+    if(g == null) return "NIL"
+    c(g.getLatitude.toString) + "|" + c(g.getLongitude.toString)
+  }
 
-    def c(s: String): String = {
-      if(s == null) return "NIL"
-      if(s.length() == 0) return "NIL"
-      s.replaceAll("[\\t\\n\\r]+", " ")
-    }
+  def c(s: String): String = {
+    if(s == null) return "NIL"
+    if(s.length() == 0) return "NIL"
+    s.replaceAll("[\\t\\n\\r]+", " ")
+  }
 
-    def main(args: Seq[String]) = {
-      val threshold = 5
-      val ratedIndividualsFiles = "/edu/arizona/sista/twitter4food/rated_individuals.csv"
-      val bufferedSource = io.Source.fromURL(ratedIndividualsFiles)
-      val userWeight = (for (line <- bufferedSource.getLines) yield {
-        val Array(userHandle, label) = line.split(",").map(_.trim)
-        userHandle -> label
-      }).toMap
-      bufferedSource.close
-      val userHandles = userWeight.keys
-      var number = 0
+  def main(args: Seq[String]) = {
+    val threshold = 5
+    val ratedIndividualsFiles = "/edu/arizona/sista/twitter4food/rated_individuals.csv"
+    val bufferedSource = io.Source.fromURL(ratedIndividualsFiles)
+    val userWeight = (for (line <- bufferedSource.getLines) yield {
+      val Array(userHandle, label) = line.split(",").map(_.trim)
+      userHandle -> label
+    }).toMap
+    bufferedSource.close
+    val userHandles = userWeight.keys
+    var number = 0
 
-      for(userHandle <- userHandles){
-        number += 1
-        val weight = userWeight(userHandle)
-        println("userHandle=" + userHandle + " weight=" + weight)
-        println("number=" + number)
+    for(userHandle <- userHandles){
+      number += 1
+      val weight = userWeight(userHandle)
+      println("userHandle=" + userHandle + " weight=" + weight)
+      println("number=" + number)
 
-        val directoryRoot = "/data/nlp/corpora/twitter4food/ratedIndividuals/"
-        val fileRoot = new File(directoryRoot)
-        if(!fileRoot.exists()){
-          fileRoot.mkdir()
-        }
-        val directory = directoryRoot + weight
-        val file = new File(directory)
-        if(!file.exists()){
-          file.mkdir()
-        }
-        val output = directory + "/" + userHandle + ".txt"
-        val userfile = new File(output)
-        if(!userfile.exists()){
-          val pw = new PrintWriter(new FileOutputStream(output, true))
-          val cb = new ConfigurationBuilder()
-          cb.setDebugEnabled(false)
-          // app name: search4food ***** Twitter4Food
-          //first
+      val directoryRoot = "/data/nlp/corpora/twitter4food/ratedIndividuals/"
+      val fileRoot = new File(directoryRoot)
+      if(!fileRoot.exists()){
+        fileRoot.mkdir()
+      }
+      val directory = directoryRoot + weight
+      val file = new File(directory)
+      if(!file.exists()){
+        file.mkdir()
+      }
+      val output = directory + "/" + userHandle + ".txt"
+      val userfile = new File(output)
+      if(!userfile.exists()){
+        val pw = new PrintWriter(new FileOutputStream(output, true))
+        val cb = new ConfigurationBuilder()
+        cb.setDebugEnabled(false)
+        // app name: search4food ***** Twitter4Food
+        //first
 
-          //      cb.setOAuthConsumerKey("")
-          //      cb.setOAuthConsumerSecret("")
-          //      cb.setOAuthAccessToken("")
-          //      cb.setOAuthAccessTokenSecret("")
+        //      cb.setOAuthConsumerKey("")
+        //      cb.setOAuthConsumerSecret("")
+        //      cb.setOAuthAccessToken("")
+        //      cb.setOAuthAccessTokenSecret("")
 
-          //fail
-          //      cb.setOAuthConsumerKey("")
-          //      cb.setOAuthConsumerSecret("")
-          //      cb.setOAuthAccessToken("")
-          //      cb.setOAuthAccessTokenSecret("")
-
-
-          //    cb.setOAuthConsumerKey("")
-          //    cb.setOAuthConsumerSecret("")
-          //    cb.setOAuthAccessToken("")
-          //    cb.setOAuthAccessTokenSecret("")
-
-          //
-          //    cb.setOAuthConsumerKey("")
-          //    cb.setOAuthConsumerSecret("")
-          //    cb.setOAuthAccessToken("")
-          //    cb.setOAuthAccessTokenSecret("")
-
-          //
-          //      cb.setOAuthConsumerKey("")
-          //      cb.setOAuthConsumerSecret("")
-          //      cb.setOAuthAccessToken("")
-          //      cb.setOAuthAccessTokenSecret("")
+        //fail
+        //      cb.setOAuthConsumerKey("")
+        //      cb.setOAuthConsumerSecret("")
+        //      cb.setOAuthAccessToken("")
+        //      cb.setOAuthAccessTokenSecret("")
 
 
-          //       app name: search4food2
-          //      cb.setOAuthConsumerKey("")
-          //      cb.setOAuthConsumerSecret("")
-          //      cb.setOAuthAccessToken("")
-          //      cb.setOAuthAccessTokenSecret("")
+        //    cb.setOAuthConsumerKey("")
+        //    cb.setOAuthConsumerSecret("")
+        //    cb.setOAuthAccessToken("")
+        //    cb.setOAuthAccessTokenSecret("")
+
+        //
+        //    cb.setOAuthConsumerKey("")
+        //    cb.setOAuthConsumerSecret("")
+        //    cb.setOAuthAccessToken("")
+        //    cb.setOAuthAccessTokenSecret("")
+
+        //
+        //      cb.setOAuthConsumerKey("")
+        //      cb.setOAuthConsumerSecret("")
+        //      cb.setOAuthAccessToken("")
+        //      cb.setOAuthAccessTokenSecret("")
 
 
-          //for DELL computer
-          //	       cb.setOAuthConsumerKey("")
-          //   cb.setOAuthConsumerSecret("")
-          //   cb.setOAuthAccessToken("")
-          //   cb.setOAuthAccessTokenSecret("")
-          ////
-          //
+        //       app name: search4food2
+        //      cb.setOAuthConsumerKey("")
+        //      cb.setOAuthConsumerSecret("")
+        //      cb.setOAuthAccessToken("")
+        //      cb.setOAuthAccessTokenSecret("")
 
 
-          //   xps 0
-          cb.setOAuthConsumerKey("")
-          cb.setOAuthConsumerSecret("")
-          cb.setOAuthAccessToken("")
-          cb.setOAuthAccessTokenSecret("")
+        //for DELL computer
+        //	       cb.setOAuthConsumerKey("")
+        //   cb.setOAuthConsumerSecret("")
+        //   cb.setOAuthAccessToken("")
+        //   cb.setOAuthAccessTokenSecret("")
+        ////
+        //
 
 
-          //xps 1
-          //	         cb.setOAuthConsumerKey("")
-          //   cb.setOAuthConsumerSecret("")
-          //   cb.setOAuthAccessToken("")
-          //   cb.setOAuthAccessTokenSecret("")
-          //
-          ////
+        //   xps 0
+        cb.setOAuthConsumerKey("")
+        cb.setOAuthConsumerSecret("")
+        cb.setOAuthAccessToken("")
+        cb.setOAuthAccessTokenSecret("")
 
 
-          //xps 2
-          //	  	         cb.setOAuthConsumerKey("")
-          //   cb.setOAuthConsumerSecret("")
-          //   cb.setOAuthAccessToken("")
-          //   cb.setOAuthAccessTokenSecret("")
-          //
+        //xps 1
+        //	         cb.setOAuthConsumerKey("")
+        //   cb.setOAuthConsumerSecret("")
+        //   cb.setOAuthAccessToken("")
+        //   cb.setOAuthAccessTokenSecret("")
+        //
+        ////
 
 
-          //xps3
-          //	  	  	         cb.setOAuthConsumerKey("")
-          //   cb.setOAuthConsumerSecret("")
-          //   cb.setOAuthAccessToken("")
-          //   cb.setOAuthAccessTokenSecret("")
+        //xps 2
+        //	  	         cb.setOAuthConsumerKey("")
+        //   cb.setOAuthConsumerSecret("")
+        //   cb.setOAuthAccessToken("")
+        //   cb.setOAuthAccessTokenSecret("")
+        //
 
 
-          //   xps 4
-          //       cb.setOAuthConsumerKey("")
-          //cb.setOAuthConsumerSecret("")
-          //cb.setOAuthAccessToken("")
-          //cb.setOAuthAccessTokenSecret("")
+        //xps3
+        //	  	  	         cb.setOAuthConsumerKey("")
+        //   cb.setOAuthConsumerSecret("")
+        //   cb.setOAuthAccessToken("")
+        //   cb.setOAuthAccessTokenSecret("")
 
 
-          //   xps 5
-          //      cb.setOAuthConsumerKey("")
-          //cb.setOAuthConsumerSecret("")
-          //cb.setOAuthAccessToken("")
-          //cb.setOAuthAccessTokenSecret("")
+        //   xps 4
+        //       cb.setOAuthConsumerKey("")
+        //cb.setOAuthConsumerSecret("")
+        //cb.setOAuthAccessToken("")
+        //cb.setOAuthAccessTokenSecret("")
 
 
-          //   xps 6
-          //      cb.setOAuthConsumerKey("")
-          //cb.setOAuthConsumerSecret("")
-          //cb.setOAuthAccessToken("")
-          //cb.setOAuthAccessTokenSecret("")
+        //   xps 5
+        //      cb.setOAuthConsumerKey("")
+        //cb.setOAuthConsumerSecret("")
+        //cb.setOAuthAccessToken("")
+        //cb.setOAuthAccessTokenSecret("")
 
 
-          //   xps 7
-          //   cb.setOAuthConsumerKey("")
-          //cb.setOAuthConsumerSecret("")
-          //cb.setOAuthAccessToken("")
-          //cb.setOAuthAccessTokenSecret("")
+        //   xps 6
+        //      cb.setOAuthConsumerKey("")
+        //cb.setOAuthConsumerSecret("")
+        //cb.setOAuthAccessToken("")
+        //cb.setOAuthAccessTokenSecret("")
 
 
-          //   xps 8
-          //      cb.setOAuthConsumerKey("")
-          //   cb.setOAuthConsumerSecret("")
-          //   cb.setOAuthAccessToken("")
-          //   cb.setOAuthAccessTokenSecret("")
-
-          //   xps 9
-          //    	      cb.setOAuthConsumerKey("")
-          //    	   cb.setOAuthConsumerSecret("")
-          //    	   cb.setOAuthAccessToken("")
-          //    	   cb.setOAuthAccessTokenSecret("")
+        //   xps 7
+        //   cb.setOAuthConsumerKey("")
+        //cb.setOAuthConsumerSecret("")
+        //cb.setOAuthAccessToken("")
+        //cb.setOAuthAccessTokenSecret("")
 
 
+        //   xps 8
+        //      cb.setOAuthConsumerKey("")
+        //   cb.setOAuthConsumerSecret("")
+        //   cb.setOAuthAccessToken("")
+        //   cb.setOAuthAccessTokenSecret("")
 
-          //  	   //   xps 10
-          //     	      cb.setOAuthConsumerKey("")
-          //     	   cb.setOAuthConsumerSecret("")
-          //     	   cb.setOAuthAccessToken("")
-          //     	   cb.setOAuthAccessTokenSecret("")
-          //
+        //   xps 9
+        //    	      cb.setOAuthConsumerKey("")
+        //    	   cb.setOAuthConsumerSecret("")
+        //    	   cb.setOAuthAccessToken("")
+        //    	   cb.setOAuthAccessTokenSecret("")
 
 
-          println("Getting Tweets: ")
 
-          ////////////////Right here the program really starts - Gets the user tweets////
+        //  	   //   xps 10
+        //     	      cb.setOAuthConsumerKey("")
+        //     	   cb.setOAuthConsumerSecret("")
+        //     	   cb.setOAuthAccessToken("")
+        //     	   cb.setOAuthAccessTokenSecret("")
+        //
 
-          val twitter: Twitter = new TwitterFactory(cb.build()).getInstance()
-          for (i <- 1 to 16) {//get the first i pages of 200 tweets (we expect i*200 tweets), max 3200 total
-          val paging = new Paging(i, 200); //200 is the max # of tweets per page
-          val statuses = twitter.getUserTimeline(userHandle, paging)
-            if(statuses!=null){
-              val u = statuses.get(0).getUser()
-              val uCreatedAt = dateToString(u.getCreatedAt())
-              for (status: Status <- statuses) {
-                pw.println(
-                  "@" + u.getScreenName() + "\t" +
-                    c(u.getName()) + "\t" +
-                    c(u.getId().toString) + "\t" +
+
+        println("Getting Tweets: ")
+
+        ////////////////Right here the program really starts - Gets the user tweets////
+
+        val twitter: Twitter = new TwitterFactory(cb.build()).getInstance()
+        for (i <- 1 to 16) {//get the first i pages of 200 tweets (we expect i*200 tweets), max 3200 total
+        val paging = new Paging(i, 200); //200 is the max # of tweets per page
+        val statuses = twitter.getUserTimeline(userHandle, paging)
+          if(statuses!=null){
+            val u = statuses.get(0).getUser()
+            val uCreatedAt = dateToString(u.getCreatedAt())
+            for (status <- asScalaBuffer(statuses)) {
+              pw.println(
+                "@" + u.getScreenName() + "\t" +
+                  c(u.getName()) + "\t" +
+                  c(u.getId().toString) + "\t" +
                   c(u.getLocation()) + "\t" +
                   c(u.getFollowersCount().toString) + "\t" +
                   c(u.getUtcOffset().toString) + "\t" +
@@ -232,14 +234,14 @@ class ScrapeIndividuals {
                   geoLocationToString(status.getGeoLocation()) + "\t" +
                   placeToString(status.getPlace()) + "\n" +
                   c(status.getText()))
-                pw.flush()
-              }
+              pw.flush()
             }
-            Thread.sleep(SLEEP)
           }
+          Thread.sleep(SLEEP)
         }
       }
     }
   }
 }
+
 
