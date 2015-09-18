b1c2d07cff219c6f8f9bf1017423c515c0b2c2ff
Dane Bell <dane@email.arizona.edu>
2015-09-18 15:20:10 -0700

    Make it extra clear we want scala.io

--- src/main/scala/edu/arizona/sista/twitter4food/ScrapeIndividuals.scala
+++ src/main/scala/edu/arizona/sista/twitter4food/ScrapeIndividuals.scala
@@ -45,7 +45,7 @@ object ScrapeIndividuals {
 
   def main(args: Array[String]) = {
     val ratedIndividualsFiles = "/edu/arizona/sista/twitter4food/rated_individuals.csv"
-    val bufferedSource = io.Source.fromURL(ratedIndividualsFiles)
+    val bufferedSource = scala.io.Source.fromURL(ratedIndividualsFiles)
     val userWeight = (for (line <- bufferedSource.getLines) yield {
       val Array(userHandle, label) = line.split(",").map(_.trim)
       userHandle -> label
