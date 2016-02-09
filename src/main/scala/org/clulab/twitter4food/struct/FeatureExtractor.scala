de4a4664141ac3bf0e1630e7bc733e43d1b4fcfe
Terron Ishihara <terron.ishihara@gmail.com>
2016-02-09 12:56:59 -0700

    Iterating on skeleton code
    Adjusted subclassifier type to generalize, added FeatureExtractor and
    T2DMClassifier skeletons

---
+++ src/main/scala/org/clulab/twitter4food/struct/FeatureExtractor.scala
@@ -0,0 +1,34 @@
+package org.clulab.twitter4food.struct
+
+import edu.arizona.sista.twitter4food.LDA
+
+/**
+  * Created by Terron on 2/9/16.
+  */
+class FeatureExtractor {
+    // TODO: understand LDA implementation
+//    val lda = new LDA
+    def ngrams(n: Int, account: TwitterAccount): Seq[String] = {
+        null
+    }
+
+    def topics(account: TwitterAccount): Seq[String] = {
+        null
+    }
+
+    def dictionaries(account: TwitterAccount): Unit = {
+        null
+    }
+
+    def embeddings(account: TwitterAccount): Map[TwitterAccount, Array[Float]] = {
+        null
+    }
+
+    // TODO: perhaps network should be handled by classifier?
+    // Network can be accessed recursively and other features can be extracted from there
+//    def network(account: TwitterAccount): Unit = {
+//
+//    }
+
+    // TODO: higher-level features, to be extracted from specific feature classifiers, also handled in meta classifier?
+}
