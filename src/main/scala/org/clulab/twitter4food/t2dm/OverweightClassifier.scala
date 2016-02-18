497584746375f06137d6e8eebc2eea25f2d1d62d
Terron Ishihara <terron.ishihara@gmail.com>
2016-02-17 23:42:46 -0700

    Added more data to overweight accounts
    Ran method to fetch accounts in overweight data to get description plus
    other account data

--- src/main/scala/org/clulab/twitter4food/t2dm/OverweightClassifier.scala
+++ src/main/scala/org/clulab/twitter4food/t2dm/OverweightClassifier.scala
@@ -4,6 +4,9 @@ import edu.arizona.sista.learning.{LinearSVMClassifier, RVFDataset}
 import edu.arizona.sista.struct.Counter
 import org.clulab.twitter4food.featureclassifier.FeatureClassifier
 import org.clulab.twitter4food.struct.{FeatureExtractor, TwitterAccount}
+import org.clulab.twitter4food.twitter4j.TwitterAPI
+
+import scala.io.Source
 
 /**
   * Created by Terron on 2/15/16.
@@ -38,8 +41,78 @@ class OverweightClassifier extends FeatureClassifier {
 }
 
 object OverweightClassifier {
+
+    val api = new TwitterAPI(0)
+
     def main(args: Array[String]) {
         val oc = new OverweightClassifier()
+
+        var numOverweight = 0
+        var numNotOverweight = 0
+
+        for (line <- Source.fromFile("src/main/resources/org/clulab/twitter4food/featureclassifier/overweight/overweightData.txt").getLines){
+            val tuple = line.split("\t")
+            val classification = tuple(1)
+            if (classification equals "Overweight")
+                numOverweight += 1
+            else if (classification equals "Not overweight")
+                numNotOverweight += 1
+        }
+
+        // Get list of handles to analyze
+        var trainAccounts: List[TwitterAccount] = List()
+        var trainLabels: List[String] = List()
+        val trainPercent = 0.80
+
+        val percentOverweight = 0.50
+
+        var testAccounts: List[TwitterAccount] = List()
+        var testLabels: List[String] = List()
+
+        println("Reading in handles from data file...")
+
+        var classification = ""
+
+        var handle = ""
+        var id = 0L
+        var name = ""
+        var lang = ""
+        var location = ""
+        var description = ""
+
+        var first = true
+
+        for (line <- Source.fromFile("src/main/resources/org/clulab/twitter4food/featureclassifier/overweight/overweightDataVerbose.txt").getLines){
+            if ( (line equals "Overweight") || (line equals "Not overweight") ) {
+                description = ""
+                classification = line
+                if (!first) {
+//                    if () {
+//
+//                    }
+                }
+            }
+            else if (line.split("\t").length > 3) {
+                var attr = line.split("\t")
+                var handle = attr(0)
+                var id = attr(1)
+                var name = attr(2)
+                var lang = attr(3)
+                var location = attr(4)
+            }
+            else {
+                description += line
+            }
+        }
+
+        println("Training accounts...")
+        oc.train(trainAccounts, trainLabels)
+
+        println("Results:")
+        println("Predicted\tActual")
+        for (i <- testAccounts.indices) {
+            println(s"${oc.classify(testAccounts(i))}\t${testLabels(i)}")
+        }
 
     }
 }
