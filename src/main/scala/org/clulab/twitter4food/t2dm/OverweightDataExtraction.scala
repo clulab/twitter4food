a6fe8c740005aed6eb8aad0dccb17a4c72391830
Terron Ishihara <terron.ishihara@gmail.com>
2016-02-23 21:07:52 -0700

    Updated OverweightDataExtraction
    Parallelized process of also extracting tweets from accounts

--- src/main/scala/org/clulab/twitter4food/t2dm/OverweightDataExtraction.scala
+++ src/main/scala/org/clulab/twitter4food/t2dm/OverweightDataExtraction.scala
@@ -14,49 +14,70 @@ import scala.io.Source
 object OverweightDataExtraction {
     def main(args: Array[String]) {
 
-        var keySet: Int = null
+        val numProcesses = 16
+
+        // Parse keySet value from args
+        var keySet: Int = -1
         if (args.length == 1) {
             keySet = args(0).toInt
-            if (keySet < 0 || keySet > 16) {
-                println("keySet must be in range [0,15]")
-                System.exit(1)
-            }
         } else {
             println("Usage: OverweightDataExtraction [keySetValue]")
             System.exit(1)
         }
-        if (keySet == null)
-            println("Error in parsing keySet value")
+
+        // Check to make sure keySet is valid
+        if (keySet < 0 || keySet > numProcesses-1) {
+            println(s"keySet must be in range [0,${numProcesses-1}]")
             System.exit(1)
+        }
 
-        val writer = new PrintWriter("src/main/resources/org/clulab/twitter4food/featureclassifier/overweight/overweightData_"+keySet+".txt")
+        println("OverweightDataExtraction: Instantiating PrintWriter...")
+        val writer = new PrintWriter("src/main/resources/org/clulab/twitter4food/featureclassifier/overweight/overweightData_" + keySet + ".txt")
 
+        println("OverweightDataExtraction: Creating instance of TwitterAPI...")
         val api = new TwitterAPI(keySet)
 
+        println("OverweightDataExtraction: Calculating total number of accounts...")
+        // Find the total number of lines to parse
+        var numLines = 0
+        for (line <- Source.fromFile("src/main/resources/org/clulab/twitter4food/featureclassifier/overweight/overweightData.txt").getLines)
+            numLines += 1
+
+        val window = numLines / numProcesses
+
+        println("OverweightDataExtraction: Iterating over accounts...")
         var i = 0
-        for (line <- Source.fromFile("src/main/resources/org/clulab/twitter4food/featureclassifier/overweight/overweightData.txt").getLines){
-            // Parse line
-            val tuple = line.split("\t")
-            val handle = tuple(0).substring(1) // ignore beginning '@'
-            val classification = tuple(1)
+        for (line <- Source.fromFile("src/main/resources/org/clulab/twitter4food/featureclassifier/overweight/overweightData.txt").getLines) {
+            // Only process the lines in this window
+            if ( i >= keySet * window && i < (keySet + 1) * window ) {
+                // Parse line
+                val tuple = line.split("\t")
+                val handle = tuple(0).substring(1) // ignore beginning '@'
+                val classification = tuple(1)
 
-            var account: TwitterAccount = null
-            try {
-                account = api.fetchAccount(handle, true, false) // fetchTweets is true
-            } catch {
-                case te: TwitterException => // ignore suspended accounts
-            }
+                var account: TwitterAccount = null
+                try {
+                    account = api.fetchAccount(handle, true, false) // fetchTweets is true
+                } catch {
+                    case te: TwitterException => // ignore suspended accounts
+                }
+                // Only include accounts that are in English
+                if (account != null && (account.lang equals "en")) {
+                    writer.write(s"C|${classification}\n")
+                    writer.write(s"A|${account.handle}\t${account.id}\t${account.name}\t${account.lang}\t${account.location}\n")
+                    writer.write(s"D|${account.description}\n")
+                    account.tweets.foreach(tweet =>
+                        if (tweet.lang equals "en")
+                            writer.write(s"T|${tweet.createdAt}\t${tweet.id}\t${tweet.text}\n"))
+                }
 
-            if (account != null) {
-                writer.write(s"${classification}\n")
-                writer.write(s"${account.handle}\t${account.id}\t${account.name}\t${account.lang}\t${account.location}\n")
-                writer.write(s"${account.description}\n")
+                println(s"Processed line ${i}")
             }
 
             i += 1
-
-            println(s"Processed line ${i}")
         }
+
+        println("\n\nOverweightDataExtraction: Finished!")
 
         writer.close
     }
