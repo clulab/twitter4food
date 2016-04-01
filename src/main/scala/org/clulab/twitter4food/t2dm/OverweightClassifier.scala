5d9865a6b71f16b140893056ef7d755d47f2cb65
Terron Ishihara <terron.ishihara@gmail.com>
2016-04-01 01:25:39 -0700

    Added analysis to OverweightClassifier

--- src/main/scala/org/clulab/twitter4food/t2dm/OverweightClassifier.scala
+++ src/main/scala/org/clulab/twitter4food/t2dm/OverweightClassifier.scala
@@ -7,7 +7,7 @@ import edu.arizona.sista.learning.{LiblinearClassifier, LinearSVMClassifier, RVF
 import edu.arizona.sista.struct.Counter
 import org.clulab.twitter4food.featureclassifier.FeatureClassifier
 import org.clulab.twitter4food.struct.{FeatureExtractor, Tweet, TwitterAccount}
-import org.clulab.twitter4food.util.{Eval, FileUtils, TestUtils}
+import org.clulab.twitter4food.util.{Eval, EvalMetric, FileUtils, TestUtils}
 
 /**
   * Created by Terron on 2/15/16.
@@ -19,7 +19,7 @@ class OverweightClassifier(val useUnigrams: Boolean = true,
                            val useEmbeddings: Boolean = false) extends FeatureClassifier {
 
     val featureExtractor = new FeatureExtractor(useUnigrams, useBigrams, useTopics, useDictionaries, useEmbeddings)
-    var subClassifier = new LinearSVMClassifier[String, String]()
+    var subClassifier : Option[LiblinearClassifier[String, String]] = None
     var dataset = new RVFDataset[String, String]()
 
     override def train(accounts: Seq[TwitterAccount], labels: Seq[String]): Unit = {
@@ -27,6 +27,7 @@ class OverweightClassifier(val useUnigrams: Boolean = true,
 
         // Clear current dataset if training on new one
         dataset = new RVFDataset[String, String]()
+        subClassifier = Some(new LinearSVMClassifier[String, String]())
 
         val pb = new me.tongfei.progressbar.ProgressBar("train()", 100)
         pb.start()
@@ -40,11 +41,31 @@ class OverweightClassifier(val useUnigrams: Boolean = true,
         }
 
         pb.stop()
-        subClassifier.train(dataset)
+        subClassifier.get.train(dataset)
     }
 
     override def scoresOf(account: TwitterAccount): Counter[String] = {
-        subClassifier.scoresOf(featureExtractor.mkDatum(account, "unknown"))
+        subClassifier.get.scoresOf(featureExtractor.mkDatum(account, "unknown"))
+    }
+
+    def analyze(modelFile: String, labels: Set[String], test: TwitterAccount) = {
+        val c = LiblinearClassifier.loadFrom[String, String](modelFile)
+        val W = c.getWeights()
+        val d = featureExtractor.mkDatum(test, "unknown")
+        val counter = d.featuresCounter
+
+        val topWeights = labels.foldLeft(Map[String, Seq[(String, Double)]]())(
+            (map, l) => map + (l -> W.get(l).get.toSeq.sortWith(_._2 > _._2)))
+
+        val dotProduct = labels.foldLeft(Map[String, Seq[(String, Double)]]())(
+            (map, l) => {
+                val weightMap = W.get(l).get.toSeq.toMap
+                val feats = d.featuresCounter.toSeq
+                map + (l -> feats.filter(f => weightMap.contains(f._1))
+                    .map(f => (f._1, f._2 * weightMap(f._1))).sortWith(_._2 > _._2))
+            })
+
+        (topWeights, dotProduct)
     }
 }
 
@@ -63,7 +84,7 @@ object OverweightClassifier {
         if (Files.exists(Paths.get(modelFile))) {
             println("Loading model from file...")
             val cl = LiblinearClassifier.loadFrom[String, String](modelFile)
-            oc.subClassifier = new LinearSVMClassifier[String, String](C=cl.C, eps=cl.eps, bias=cl.bias)
+            oc.subClassifier = Some(cl)
         } else {
             println("Loading training accounts...")
             val trainingData = FileUtils.load(config.getString("classifiers.overweight.trainingData"))
@@ -71,10 +92,8 @@ object OverweightClassifier {
             // Train classifier and save model to file
             println("Training classifier...")
             oc.train(trainingData.keys.toSeq, trainingData.values.toSeq)
-            oc.subClassifier.saveTo(modelFile)
+            oc.subClassifier.get.saveTo(modelFile)
         }
-
-        println(s"${oc.subClassifier.getWeights()}")
 
         println("Loading dev accounts...")
         val devData = FileUtils.load(config.getString("classifiers.overweight.devData"))
@@ -93,20 +112,25 @@ object OverweightClassifier {
 
         pb.stop()
 
-        println("Dev:")
-        println(devLabels.mkString("\t"))
-        println("\nPredicted:")
-        println(predictedLabels.mkString("\t"))
-
+        // Print results
         val (evalMeasures, microAvg, macroAvg) = Eval.evaluate(devLabels, predictedLabels,
             devData.keys.toSeq)
 
-        println(evalMeasures.mkString("\n"))
-
         val evalMetric = evalMeasures("Overweight")
-
         val precision = evalMetric.P
         val recall = evalMetric.R
+
+        println("False negatives:")
+        evalMetric.FNAccounts.map(account => print(account.handle + "\t"))
+        println("\n====")
+        outputAnalysis(config.getString("classifier") + "/overweight/results/errorAnalysisFN" +
+            fileExt + ".txt", modelFile, "*** False negatives ***\n\n", evalMetric.FNAccounts, oc)
+
+        println("False positives:")
+        evalMetric.FNAccounts.map(account => print(account.handle + "\t"))
+        println("\n====")
+        outputAnalysis(config.getString("classifier") + "/overweight/results/errorAnalysisFP" +
+            fileExt + ".txt", modelFile, "*** False positives ***\n\n", evalMetric.FNAccounts, oc)
 
         println("\nResults:")
         println(s"Precision: ${precision}")
@@ -119,7 +143,7 @@ object OverweightClassifier {
         // Save results
         val writer = new BufferedWriter(new FileWriter(
             config.getString("classifier") + "/overweight/results/output" +
-                fileExt + ".txt",true))
+                fileExt + ".txt", false))
         writer.write(s"Precision: ${precision}\n")
         writer.write(s"Recall: ${recall}\n")
         writer.write(s"F-measure (harmonic mean): ${fMeasure(precision, recall, 1)}\n")
@@ -128,6 +152,49 @@ object OverweightClassifier {
         writer.write(s"Micro average: ${microAvg}")
         writer.close()
 
+    }
+
+    private def outputAnalysis(outputFile:String, modelFile: String, header:String, accounts: Seq[TwitterAccount], oc: OverweightClassifier): Unit = {
+        // Set progress bar
+        val pb = new me.tongfei.progressbar.ProgressBar("outputAnalysis()", 100)
+        pb.start()
+        pb.maxHint(accounts.size)
+        pb.setExtraMessage(header)
+
+        val writer = new BufferedWriter(new FileWriter(outputFile, false))
+        var isFirst = true
+        writer.write(header)
+        for (account <- accounts) {
+            val (topWeights, dotProduct) = oc.analyze(modelFile, Set[String]("Overweight"), account)
+            if (isFirst) {
+                writer.write("Top weights:\n")
+                for ((label, sequence) <- topWeights) {
+                    var numToPrint = 50
+                    for ((feature, score) <- sequence) {
+                        if ((numToPrint > 0) && (score > 0.0)) {
+                            writer.write(s"${feature} -> ${score}\n")
+                            numToPrint = numToPrint - 1
+                        }
+                    }
+                }
+                isFirst = false
+                writer.write("================================\n")
+            }
+            writer.write(s"Hadamard product for ${account.handle}:\n")
+            for ((label, sequence) <- dotProduct) {
+                var numToPrint = 50
+                for ((feature, score) <- sequence) {
+                    if ((numToPrint > 0) && (score > 0.0)) {
+                        writer.write(s"${feature} -> ${score}\n")
+                        numToPrint = numToPrint - 1
+                    }
+                }
+            }
+            writer.write("================================\n")
+            pb.step()
+        }
+        writer.close
+        pb.stop()
     }
 
     private def fMeasure(precision: Double, recall: Double, beta: Double): Double =
