b73a63abe01a022cd790c3d8574130f8078eb100
Aditya Kousik <adit267.kousik@gmail.com>
2016-06-06 08:28:56 -0500

    Updated overweight handle extraction to group by hashtag.

--- src/test/scala/org/clulab/twitter4food/TestDownloadAccounts.scala
+++ src/test/scala/org/clulab/twitter4food/TestDownloadAccounts.scala
@@ -6,10 +6,10 @@ object TestDownloadAccounts {
   def main(args: Array[String]): Unit = {
     val keyset = args(0).toInt
     val numWindows = args(1).toInt
-    val (api, config) = TestUtils.init(keyset, true)
+    val (api, config) = TestUtils.init(keyset)
     val hlMap = TestUtils.loadHandles(s"${config.getString("classifier")}/overweight/ow_accounts.txt")
     val (subH, subL) = TestUtils.splitHandles(keyset, numWindows, hlMap)
-    val accounts = TestUtils.fetchAccounts(api, subH, true)
+    val accounts = TestUtils.fetchAccounts(api, subH, true, true, true)
 
     FileUtils.saveToFile(accounts, subL, 
       config.getString("classifiers.overweight.opt")+keyset+".txt")
