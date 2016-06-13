1e4bbc4273a5eb3956e80e94a7869eec399ef9f2
Dane Bell <dane@email.arizona.edu>
2016-06-13 12:57:33 -0700

    Complete normalization in place

--- src/main/scala/org/clulab/twitter4food/featureclassifier/DatasetNormalization.scala
+++ src/main/scala/org/clulab/twitter4food/featureclassifier/DatasetNormalization.scala
@@ -1,36 +1,29 @@
 package org.clulab.twitter4food.featureclassifier
 
-import edu.arizona.sista.learning.{RVFDataset, RVFDatum}
+import edu.arizona.sista.learning.{RVFDataset, RVFDatum, ScaleRange}
 import edu.arizona.sista.struct.Counter
 
-abstract class DatasetNormalization {
-  def normalize[L,F](dataset: RVFDataset[L,F]): RVFDataset[L,F]
-}
+object DatasetNormalization {
+  /** The actual scaling formula taken from svm-scale
+    * Copied from edu.arizona.sista.learning.Datasets
+    * */
+  private def scale(value:Double, min:Double, max:Double, lower:Double, upper:Double):Double = {
+    if(min == max) return upper
 
-class NoNormalization extends DatasetNormalization {
-  def normalize[L,F](dataset: RVFDataset[L,F]): RVFDataset[L,F] = dataset
-}
-
-class DatumNormalization extends DatasetNormalization {
-  def labelledFeaturesCounter[L,F](dataset: RVFDataset[L,F], datumOffset:Int):Counter[F] = {
-    val c = new Counter[F]
-    val fs = dataset.features(datumOffset).map(l => dataset.featureLexicon.get(l))
-    val vs = dataset.values(datumOffset)
-    for(i <- fs.indices) {
-      c.incrementCount(fs(i), vs(i))
-    }
-    c
+    // the result will be a value in [lower, upper]
+    lower + (upper - lower) * (value - min) / (max - min)
   }
 
-  def normalize[L,F](dataset: RVFDataset[L,F]): RVFDataset[L,F] = {
-    val normalized = new RVFDataset[L,F]()
-    for (i <- dataset.indices) {
-      val fc = labelledFeaturesCounter(dataset, i)
-      val normed = fc / fc.l1Norm
-      normalized += new RVFDatum[L,F](dataset.labelLexicon.get(dataset.labels(i)), normed)
+  def scaleByDatum[L, F](dataset:RVFDataset[L, F], lower:Double, upper:Double): Unit = {
+    // scan the dataset once and keep track of min/max for each feature
+    for(i <- dataset.indices) {
+      val datumMin = dataset.values(i).min
+      val datumMax = dataset.values(i).max
+      for(j <- dataset.features(i).indices) {
+        dataset.values(i)(j) = scale(dataset.values(i)(j), datumMin, datumMax, lower, upper)
+      }
     }
-    normalized
   }
-}
 
+}
 
