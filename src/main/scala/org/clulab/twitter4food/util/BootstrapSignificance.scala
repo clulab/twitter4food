35b611333404cfb958ae8951a930617fc4e3e383
Dane Bell <dane@email.arizona.edu>
2016-06-29 14:24:45 -0700

    More progress on bootstrapping

--- src/main/scala/org/clulab/twitter4food/util/BootstrapSignificance.scala
+++ src/main/scala/org/clulab/twitter4food/util/BootstrapSignificance.scala
@@ -12,7 +12,21 @@ import scala.util.Random
   */
 object BootstrapSignificance {
 
-  def microf1(gold: Seq[String], pred: Seq[String], )
+  def microF1(gold: Seq[String], pred: Seq[String]): Double = {
+    val tp: Double = (for (i <- gold.indices) yield if (gold(i) == pred(i)) 1 else 0).sum
+    val fn: Double = (for {
+      lbl <- gold.distinct
+      i <- gold.indices
+      if gold(i) == lbl && pred(i) != lbl
+    } yield 1).sum
+    val fp: Double = (for {
+      lbl <- gold.distinct
+      i <- gold.indices
+      if gold(i) != lbl && pred(i) == lbl
+    } yield 1).sum
+
+    2.0 * tp / (2 * tp + fn + fp)
+  }
 
   def main(args: Seq[String]) = {
     val logger = LoggerFactory.getLogger("BootstrapSignificance")
@@ -43,25 +57,34 @@ object BootstrapSignificance {
 
     // Ignore results that have different Gold annotations and thus different users or user order
     val comparable = predictionsWithGold.filter(pred => pred._2.unzip._1 == gold)
-    val incomparable = predictions.keySet diff comparable.keySet
+    val incomparable = predictionsWithGold.keySet diff comparable.keySet
     if(incomparable.nonEmpty) {
       logger.info(s"""$incomparable did not have the same gold annotations as baseline""")
     }
 
-    val predictions = comparable.map(featureSet => featureSet._1 -> featureSet._2.unzip._2)
+    // Don't bother performing calculation of baseline against itself
+    val predictions = comparable
+      .filterKeys(_ != baselineFeatures)
+      .map(featureSet => featureSet._1 -> featureSet._2.unzip._2)
 
     val betterThanBaseline: Map[String, scala.collection.mutable.ListBuffer[Double]] = (for {
       key <- comparable.keys
     } yield key -> new scala.collection.mutable.ListBuffer[Double]).toMap
 
+    // TODO: Can we make this more efficient? Indexing is on IndexedSeq, so constant time per index
     for {
       i <- 0 until reps
-      sampleIdx = for (j <- gold.indices) yield Random.nextInt(gold.length - 1)
+      sampleIdx = for (j <- gold.indices) yield Random.nextInt(gold.length - 1) // random sample with replacement
       sampleGold = for (j <- sampleIdx) yield gold(j)
-      featureSet <- predictions.keys
-      preds = predictions(featureSet)
-      sample = for (j <- sampleIdx) yield preds(j)
-    } {}
+      featureSet <- predictions.keys  // same sample applied to each eligible featureSet
+      pred = predictions(featureSet)
+      samplePred = for (j <- sampleIdx) yield pred(j) // predictions for sampled accts
+      sampleBase = for (j <- sampleIdx) yield baseline(j) // baseline predictions for sampled accts
+    } {
+      val baselineF1 = microF1(sampleGold, sampleBase)
+      val predF1 = microF1(sampleGold, samplePred)
+      betterThanBaseline(featureSet)(i) = if (predF1 > baselineF1) 1.0 else 0.0
+    }
 
   }
 }
