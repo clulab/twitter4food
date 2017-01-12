89d92ede2adb76b428e467354dbac5c632a26908
Dane Bell <dane@email.arizona.edu>
2017-01-12 15:07:44 -0700

    Union of dictionaries and best features

--- src/main/scala/org/clulab/twitter4food/struct/FeatureExtractor.scala
+++ src/main/scala/org/clulab/twitter4food/struct/FeatureExtractor.scala
@@ -339,20 +339,20 @@ class FeatureExtractor (
     */
   def ngrams(n: Int, tweets: Seq[Array[String]], description: Array[String]): Counter[String] = {
     val counter = new Counter[String]
-//    val foodWords = lexicons.get("Overweight")("activity_words")
-//    val activityWords = lexicons.get("Overweight")("food_words")
-//    val restaurantWords = lexicons.get("Overweight")("restaurant_hashtags")
-//    val owHashtags = lexicons.get("Overweight")("overweight_hashtags")
+    val foodWords = lexicons.get("Overweight")("activity_words")
+    val activityWords = lexicons.get("Overweight")("food_words")
+    val restaurantWords = lexicons.get("Overweight")("restaurant_hashtags")
+    val owHashtags = lexicons.get("Overweight")("overweight_hashtags")
     val stateFeatures = lexicons.get("Overweight")("topStateFeatures")
 
     // Extract ngrams
     def populateNGrams(n: Int, text: Array[String]): Seq[String] = {
       text
-//        .filter(w => foodWords.contains(w) ||
-//          activityWords.contains(w) ||
-//          restaurantWords.contains(w) ||
-//          owHashtags.contains(w))
-        .filter(w => stateFeatures.contains(w))
+        .filter(w => foodWords.contains(w) ||
+          activityWords.contains(w) ||
+          restaurantWords.contains(w) ||
+          owHashtags.contains(w) ||
+          stateFeatures.contains(w))
         .sliding(n)
         .toList
         .map(ngram => ngram.mkString(s"$n-gram:", " ", ""))
