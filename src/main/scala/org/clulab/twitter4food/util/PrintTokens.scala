9b2796cc0ec9ca5ef1ad8d84c198424f859f5802
Dane Bell <dane@email.arizona.edu>
2017-02-06 11:52:44 -0700

    Print tokens for cross-val

--- src/main/scala/org/clulab/twitter4food/util/PrintTokens.scala
+++ src/main/scala/org/clulab/twitter4food/util/PrintTokens.scala
@@ -18,6 +18,10 @@ object PrintTokens {
     * Tweets are pre-tokenized (so no newlines w/i text).
     */
   def writeTokens(accounts: Seq[Seq[String]], loc: String): Unit = {
+    val pb = new me.tongfei.progressbar.ProgressBar("printing", 100)
+    pb.start()
+    pb.maxHint(accounts.length)
+
     val locFile = new File(loc)
     if (!locFile.exists) locFile.mkdir()
     accounts.zipWithIndex.foreach{ case (tweets, i) =>
@@ -25,7 +29,10 @@ object PrintTokens {
       val writer = new BufferedWriter(new FileWriter(fileName))
       writer.write(tweets.mkString("\n"))
       writer.close()
+      pb.step()
     }
+
+    pb.stop()
   }
 
   /**
@@ -36,36 +43,19 @@ object PrintTokens {
     val dataset = if(args.isEmpty) "overweight" else args.head
 
     logger.info("Loading Twitter accounts")
-    val train = FileUtils.load(config.getString(s"classifiers.$dataset.trainingData")).toSeq
-    val dev = FileUtils.load(config.getString(s"classifiers.$dataset.devData")).toSeq
-    val test = FileUtils.load(config.getString(s"classifiers.$dataset.testData")).toSeq
+    val labeledAccts = FileUtils.load(config.getString(s"classifiers.$dataset.data")).toSeq
 
     logger.info("Writing tokens in LSTM-readable format")
 
-    val trainFile = new File(s"$base${sep}train")
-    if (! trainFile.exists) trainFile.mkdir()
-    val devFile = new File(s"$base${sep}dev")
-    if (! devFile.exists) devFile.mkdir()
-    val testFile = new File(s"$base${sep}test")
-    if (! testFile.exists) testFile.mkdir()
+    // Scale number of accounts so that weights aren't too biased against Overweight
+    val desiredProps = Map( "Overweight" -> 0.5, "Not overweight" -> 0.5 )
+    val subsampled = Utils.subsample(labeledAccts, desiredProps)
 
-    train.groupBy(_._2).foreach{ case (lbl, acctsWithLabels) =>
+    subsampled.groupBy(_._2).foreach{ case (lbl, acctsWithLabels) =>
       // folderNames should not contain whitespace
       val folderName = lbl.replaceAll("[^a-zA-Z0-9]+", "")
       val texts = acctsWithLabels.map(_._1.tweets.map(_.text))
-      writeTokens(texts, s"$base${sep}train$sep$folderName")
-    }
-    dev.groupBy(_._2).foreach{ case (lbl, acctsWithLabels) =>
-      // folderNames should not contain whitespace
-      val folderName = lbl.replaceAll("[^a-zA-Z0-9]+", "")
-      val texts = acctsWithLabels.map(_._1.tweets.map(_.text))
-      writeTokens(texts, s"$base${sep}dev$sep$folderName")
-    }
-    test.groupBy(_._2).foreach{ case (lbl, acctsWithLabels) =>
-      // folderNames should not contain whitespace
-      val folderName = lbl.replaceAll("[^a-zA-Z0-9]+", "")
-      val texts = acctsWithLabels.map(_._1.tweets.map(_.text))
-      writeTokens(texts, s"$base${sep}test$sep$folderName")
+      writeTokens(texts, s"$base$sep$folderName")
     }
   }
 }
