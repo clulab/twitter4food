08644af82c57af07cc3842062f3f8f62654de518
Dane Bell <dane@email.arizona.edu>
2017-05-08 11:39:13 -0700

    Sort venues by type as well as distance

--- src/main/scala/org/clulab/twitter4food/data/FindVenues.scala
+++ src/main/scala/org/clulab/twitter4food/data/FindVenues.scala
@@ -20,6 +20,10 @@ object FindVenues extends App {
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
   val sleepTime = 600 // 150000 requests / 24 hours => 576 ms per request
   val maxDist = 50 // meters between venues and tweet location
+  val toKeep = 5 // only keep 5 closest / most relevant Places
+  // A set of relevant Google Places types and FourSquare categories to compare against for relevance
+  val relevantTypes = io.Source.fromFile(config.getString("classifiers.overweight.location_types"))
+    .getLines().map(_.stripLineEnd).toSet
 
   // Locations before Venue annotation
   val coordsFile = config.getString("classifiers.overweight.tweetCoords")
@@ -38,18 +42,22 @@ object FindVenues extends App {
   val locs = for (
     coord <- coords
   ) yield {
-    val ll = new LatLng(coord.lat, coord.lng)
+    val ll = new LatLng(coord.lat, coord.lng) // LatLng to search near
     val results = PlacesApi.nearbySearchQuery(context, ll)
-      //.rankby(RankBy.DISTANCE)
-      .radius(maxDist)
-      .await()
-      .results
+      .radius(maxDist) // at most maxDist away
+      .await() // submit query
+      .results // get results of query, if any
+    // wait to avoid stepping over API rate limit
     Thread.sleep(sleepTime)
+    // Sort results by relevance and add them to coord's venues
     if (results.isEmpty) coord else {
       val venues = results.map{ result =>
         new Venue(result.name, result.types, result.geometry.location.lat, result.geometry.location.lng)
-      }.sortBy(_.dist(coord))
-      coord.copy(venues = venues)
+      }.sortBy{v =>
+        // near distance is best, but when multiple places in exact same location, pick food-related ones
+        (v.dist(coord), v.types.toSet.intersect(relevantTypes).isEmpty)
+      }.take(toKeep) // Anything past these is less likely to be relevant
+      coord.copy(venues = venues) // Everything is the same but now with venues
     }
   }
 
