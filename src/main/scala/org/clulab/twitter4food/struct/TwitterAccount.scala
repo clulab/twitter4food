e59c382585a072c69d2eecc187aa06e6cad035b1
Dane Bell <dane@email.arizona.edu>
2017-09-12 15:35:28 -0700

    Minor

--- src/main/scala/org/clulab/twitter4food/struct/TwitterAccount.scala
+++ src/main/scala/org/clulab/twitter4food/struct/TwitterAccount.scala
@@ -45,7 +45,7 @@ class TwitterAccount (
 
   def toFlat(maxTweets: Option[Int], minWds: Option[Int]): String = {
     val ts = if (maxTweets.isEmpty) this.tweets else this.tweets.slice(0, maxTweets.get)
-    val longEnough = if (minWds.isEmpty) ts else ts.filter(t => t.text.split("\\s+").length > minWds.get)
+    val longEnough = if (minWds.isEmpty) ts else ts.filter(t => t.text.split("\\s+").length >= minWds.get)
     val lines = for (t <- longEnough) yield s"${this.handle}\t${t.text}"
     lines.mkString("\n")
   }
