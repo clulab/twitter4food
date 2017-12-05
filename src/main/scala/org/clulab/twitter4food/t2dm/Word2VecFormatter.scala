664df4e915d6840aef9e2ac96dad2739e7e02df9
Dane Bell <dane@email.arizona.edu>
2017-12-05 13:27:04 -0700

    Rewrite W2V formatter

--- src/main/scala/org/clulab/twitter4food/t2dm/Word2VecFormatter.scala
+++ src/main/scala/org/clulab/twitter4food/t2dm/Word2VecFormatter.scala
@@ -3,36 +3,40 @@ package org.clulab.twitter4food.t2dm
 import java.io.{BufferedReader, FileReader, PrintWriter}
 
 import cmu.arktweetnlp.Tagger.TaggedToken
-import org.clulab.twitter4food.util.{Utils, Tokenizer}
+import org.clulab.twitter4food.util.{FileUtils, Utils, Tokenizer}
+
+case class W2VConfig(inFile: String = "", outFile: String = "", isThreeLine: Boolean = true)
 
 /**
-  * Created by Terron on 4/28/16.
-  *
-  * A simple script for parsing a file of tweets and writing to a file of aggregate text
-  * for the w2v C script. Assumes every third line is a tweet, as is standard for this project.
+  * Load a 2- or 3-line formatted Twitter file and write it in a word2vec-readable format.
   */
-object Word2VecFormatter {
-
-    def main(args: Array[String]) {
-        val (_, config) = Utils.init(0)
-        val overweightFile = scala.io.Source.fromFile(config.getString("classifiers.features.overweight_corpus"))
-
-        val outputFile = config.getString("classifiers.features.overweight_corpus_tokenized")
-        val writer = new PrintWriter(outputFile)
-
-        var i = 0
-        for (line <- overweightFile.getLines) {
-            // Line is a tweet
-            if (i % 3 == 2) {
-                val tokens = Tokenizer.annotate(line.toLowerCase).filter(tt => !("@UGD,~$".contains(tt.tag))
-                    && "#NVAT".contains(tt.tag)).map(tt => tt.token)
-                writer.write(tokens.mkString(" ") + " ")
-            }
-
-            i += 1
-            i %= 3
-        }
-        writer.close()
-        overweightFile.close()
+object Word2VecFormatter extends App {
+  def parseArgs(args: Array[String]): W2VConfig = {
+    val parser = new scopt.OptionParser[W2VConfig]("w2vFormatter") {
+      arg[String]("inFile") action { (x, c) =>
+        c.copy(inFile = x)
+      } text "which file to convert"
+      arg[String]("outFile") action { (x, c) =>
+        c.copy(outFile = x)
+      } text "where to put the converted file"
+      opt[Unit]('t', "isTwoLine") action { (x, c) =>
+        c.copy(isThreeLine = false)
+      } text "for two-line input format"
     }
+
+    parser.parse(args, W2VConfig()).get
+  }
+
+  val config = parseArgs(args)
+
+  val writer = new PrintWriter(config.outFile)
+
+  val tweets = if (config.isThreeLine)
+    FileUtils.loadThreeLineTexts(config.inFile)
+  else
+    FileUtils.loadTwoLineTexts(config.inFile)
+
+  tweets.foreach(tweet => writer.write(s"$tweet\n"))
+
+  writer.close()
 }
