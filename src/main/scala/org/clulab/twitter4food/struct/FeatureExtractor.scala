570ca681f903acfbf51df810912a1c21f8d3852e
danebell <dane@email.arizona.edu>
2018-04-01 18:23:02 -0700

    No description

--- src/main/scala/org/clulab/twitter4food/struct/FeatureExtractor.scala
+++ src/main/scala/org/clulab/twitter4food/struct/FeatureExtractor.scala
@@ -447,10 +447,10 @@ class FeatureExtractor (
 
     val denoised = if (denoise) tweets.filterNot(isNoise) else tweets
 
-    val relevantDesc = if (dictOnly) dictFilter(description) else description // only relevant words if 'dictOnly'
-    val filteredDesc = if (n == 1) filterStopWords(relevantDesc) else relevantDesc // remove stopwords
+    //val relevantDesc = if (dictOnly) dictFilter(description) else description // only relevant words if 'dictOnly'
+    //val filteredDesc = if (n == 1) filterStopWords(relevantDesc) else relevantDesc // remove stopwords
     // special prefix for description tokens since they summarize an account more than tweets
-    setCounts(tokenNGrams(n, filteredDesc, "desc"), counter) // always set n-gram counts
+    //setCounts(tokenNGrams(n, filteredDesc, "desc"), counter) // always set n-gram counts
 
     // n-gram for tweets
     denoised.foreach{ tweet =>
